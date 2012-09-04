5f93e1fe2182158cecd0bd07e3c5dae3689ba5a0
Vojtech Dobes <me@vojtechdobes.com>
2012-09-04 19:14:44 +0200

    History: extension becomes optional (moved to another file)

--- nette.ajax.js
+++ nette.ajax.js
@@ -351,26 +351,6 @@ $.nette.ext('redirect', {
 	}
 });
 
-// change URL (requires HTML5)
-if (!!(window.history && history.pushState)) { // check borrowed from Modernizr
-	$.nette.ext('history', {
-		before: function (settings, ui) {
-			var $el = $(ui);
-			if ($el.is('a')) {
-				this.href = ui.href;
-			}
-		},
-		success: function (payload) {
-			if (payload.url) {
-				this.href = payload.url;
-			}
-			if (!payload.signal && this.href) {
-				history.pushState({href: this.href}, '', this.href);
-			}
-		}
-	}, {href: null});
-}
-
 // current page state
 $.nette.ext('state', {
 	success: function (payload) {
